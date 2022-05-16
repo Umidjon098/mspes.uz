37120ca9d42310e0636371d4b5ff10a3afd3cd77
Umidjon098 <umidtuychiev098@gmail.com>
2022-05-16 22:56:28 +0500

    fix

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -6,7 +6,7 @@ import { MainContext } from "../context/MainContext";
 import {} from "@ant-design/icons";
 import Logo from "../assets/images/logo.jpg";
 import { lang } from "../util";
-import {tl} from "../configs/i18n";
+import { tl } from "../configs/i18n";
 
 const { Option } = Select;
 const Navbar = () => {
@@ -46,9 +46,7 @@ const Navbar = () => {
                         <Link to="/requirements">{tl("requirements")}</Link>
                       </Menu.Item>
                       <Menu.Item>
-                        <Link to="/instruction">
-                          {tl("instruction")}
-                        </Link>
+                        <Link to="/instruction">{tl("instruction")}</Link>
                       </Menu.Item>
                     </Menu>
                   }
@@ -65,7 +63,9 @@ const Navbar = () => {
                   overlay={
                     <Menu>
                       <Menu.Item>
-                        <Link to="/current_journal">{tl("current.journal")}</Link>
+                        <Link to="/current_journal">
+                          {tl("current.journal")}
+                        </Link>
                       </Menu.Item>
                       <Menu.Item>
                         <Link to="/archive_journal">{tl("archive")}</Link>
@@ -75,7 +75,7 @@ const Navbar = () => {
                   trigger={["click"]}
                 >
                   <Space>
-                    {tl('archive')}
+                    {tl("archive")}
                     <DownOutlined />
                   </Space>
                 </Dropdown>
@@ -131,6 +131,17 @@ const Navbar = () => {
             <Link to="/indexing">{tl("index")}</Link>
           </Menu.Item>
         </Menu>
+        <div className="language mobile">
+          {lang === "ru" ? (
+            <img src="https://img.icons8.com/color/30/000000/russian-federation.png" />
+          ) : (
+            <img src="https://img.icons8.com/officel/30/000000/kazakhstan.png" />
+          )}
+          <Select defaultValue={lang} onChange={handleChange}>
+            <Option value="ru">Русский</Option>
+            <Option value="kk">қазақ</Option>
+          </Select>
+        </div>
       </Drawer>
     </div>
   );
